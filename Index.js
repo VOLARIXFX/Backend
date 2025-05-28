8dbd0220285b66b49e2dee3dd9124b18abdf21e8
VOLARIXFX <volarixfx@gmail.com>
2025-05-28 11:16:34 -0500

    Cambios index
    para produccion

--- Index.js
+++ Index.js
@@ -6,6 +6,16 @@ import LoginUser from "./src/Routes/LoginRouters.js";
 import formularios from "./src/Routes/FormularioRouter.js";
 import estadisticas from "./src/Routes/EstadisticasRouter.js";
 import soporte from "./src/Routes/SoporteRouter.js";
+import path from "path";
+
+
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+// ✅ Esto corrige __dirname en ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename)
+
 const VolarixCapital = express()
 const PORT = process.env.PORT;
 
@@ -15,11 +25,16 @@ dotenv.config()
 
 
 
-VolarixCapital.use(cors())
+VolarixCapital.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
 VolarixCapital.use(express.json())
+VolarixCapital.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 
-VolarixCapital.use('/uploads', express.static('uploads'))
 VolarixCapital.use('/users', RouterUsers)
 VolarixCapital.use('/form', formularios)
 VolarixCapital.use('/estadisticas', estadisticas)
@@ -29,8 +44,10 @@ VolarixCapital.use(LoginUser)
 VolarixCapital.get('/', (req, res) => {
     res.send('Servidor funcionando correctamente');
 });
+VolarixCapital.get("/test", (req, res) => {
+  res.json({ mensaje: "GET funciona" });
+});
 
-VolarixCapital.listen(PORT, ()=>{
-    console.log(`Servidor corriendo ${PORT}`);
-})
-
+VolarixCapital.listen(PORT, '0.0.0.0', () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
+});
